4d63867c7409075ce3f70b73cfbd1501e1dad8be
simokhounti <larbikhounti@gmail.com>
2021-03-05 14:12:26 +0100

    adding function to add a ticket

--- functions/dataset.php
+++ functions/dataset.php
@@ -25,13 +25,13 @@ class dataset
     }
   }
   public  function authUser($motdepass, $email)
-  { 
-    try{
+  {
+    try {
       $myquery =  $this->conn->prepare("select * from voyageur where email =:email AND password=:motdepass ");
       $myquery->bindParam(":email", $email);
       $myquery->bindParam(":motdepass", $motdepass);
       $myquery->execute();
-    
+
       if ($myquery->rowcount() > 0) {
         $data = $myquery->fetch(PDO::FETCH_ASSOC);
         session_start();
@@ -43,23 +43,35 @@ class dataset
       } else {
         return 0;
       }
-    }catch(Exception $ex){
+    } catch (Exception $ex) {
       return $ex;
     }
-   
   }
 
-  public function getTravels($cityStart,$cityEnd){
-      $myquery =  $this->conn->prepare("select * from voyage where  villeDepart = :cityStart AND villeDarrivee = :cityEnd ");
-      $myquery->bindParam(":cityStart", $cityStart);
-      $myquery->bindParam(":cityEnd", $cityEnd);
-      $myquery->execute();
-      if ($myquery->rowcount() > 0) {
-        $data = $myquery->fetchAll(PDO::FETCH_ASSOC);
-        return $data;
-      } else {
-        return 0;
-      }
+  public function getTravels($cityStart, $cityEnd)
+  {
+    $myquery =  $this->conn->prepare("select * from voyage where  villeDepart = :cityStart AND villeDarrivee = :cityEnd ");
+    $myquery->bindParam(":cityStart", $cityStart);
+    $myquery->bindParam(":cityEnd", $cityEnd);
+    $myquery->execute();
+    if ($myquery->rowcount() > 0) {
+      $data = $myquery->fetchAll(PDO::FETCH_ASSOC);
+      return $data;
+    } else {
+      return 0;
+    }
   }
-
+  public function addTicket($codeVoyage, $email, $telephone)
+  {
+    $myquery =  $this->conn->prepare("insert into bilet (codeVoyage,email,telephone) values (:codeVoyage,:email,:telephone) ");
+    $myquery->bindParam(":codeVoyage", $codeVoyage);
+    $myquery->bindParam(":email", $email);
+    $myquery->bindParam(":telephone", $telephone);
+    $myquery->execute();
+    if ($myquery->execute()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
