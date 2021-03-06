fb8f26cb467a02c618b9ba878f47e236dd21ab82
simokhounti <larbikhounti@gmail.com>
2021-03-06 01:20:45 +0100

    adding logo

--- static/navbar.php
+++ static/navbar.php
@@ -2,7 +2,7 @@
 
 $navBar = " <nav class='navbar navbar-expand-lg navbar-light bg-light'>
     <div class='container-fluid  '>
-      <a class='navbar-brand' href='#'>Navbar</a>
+      <a class='navbar-brand fw-bold' href='#'>TRAVEL<span class='text-success fw-bolder'> IT.<span></a>
       <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
         <span class='navbar-toggler-icon'></span>
       </button>
